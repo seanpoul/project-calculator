9012667dc4c3d92154d9686633a596c289728c81
Sean <seanpoulter@hotmail.com>
2022-07-22 18:43:05 -0400

    Clear and delete button work.

--- script.js
+++ script.js
@@ -5,64 +5,27 @@ const clear = document.getElementById('.clear');
 const equal = document.getElementById('.equal');
 const dot = document.getElementById('.dot');
 
-let value1 = "";
-let value2 = "";
-
-let result = 0;
-let operatorValue = "";
-
-let add = (a, b) => a + b;
-let subtract = (a, b) => a - b;
-let multiply = (a, b) => a * b;
-let divide = (a, b) => {
-    if (b == 0) {
-        return 'nope';
-    }
-    else {
-        a / b;
-    }
-}
 
 allButtons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        let value = e.target.value;
-        value1 = calcAnswer.innerText;
-        calcAnswer.innerText += value;
-        getNumbers(e)
+    button.addEventListener('click', (event) => {
+        let clickedValue = event.target.value;
+        calcAnswer.textContent += clickedValue;
+        // console.log(calcAnswer.textContent);
+        if (clickedValue == "clear") {
+            calcAnswer.textContent = "";
+        }
+        if (clickedValue == "delete") {
+            let display = calcAnswer.textContent;
+            display = display.slice(0, -7);
+            calcAnswer.textContent = display;
+        }
+
+
+
+
+
+
+
     })
 })
 
-function operate() {
-    let mathOne = firstNum;
-    let mathTwo = secondNum;
-    console.log(operatorValue)
-    if (operatorValue == "+") {
-        result = add(mathOne, mathTwo);
-    }
-    if (operatorValue = "-") {
-        result = subtract(mathOne, mathTwo);
-    }
-    if (operatorValue = "*") {
-        result = multiply(mathOne, mathTwo);
-    }
-    if (operatorValue = "/") {
-        result = divide(mathOne, mathTwo);
-    }
-}
-
-let firstNum = "";
-let secondNum = "";
-
-function getNumbers(e) {
-    if (operatorValue == "") {
-        let attribute = e.target.value;
-        firstNum = firstNum + attribute;
-        calcAnswer.textContent = firstNum;
-    }
-    else {
-        let attribute = e.target.value;
-        secondNum = secondNum + attribute;
-        calcAnswer.textContent = secondNum;
-    }
-    operate()
-}
