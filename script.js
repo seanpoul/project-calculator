5fd94a71fb7fbf8d5c09a0828a5d426a438fa73b
Sean <seanpoulter@hotmail.com>
2022-07-22 19:51:36 -0400

    Dot button works.

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 const calcAnswer = document.querySelector('.answer');
 const allButtons = document.querySelectorAll('button');
-const operators = document.getElementsByClassName('.operators');
-const clear = document.getElementById('.clear');
-const equal = document.getElementById('.equal');
-const dot = document.getElementById('.dot');
+const operators = document.getElementsByClassName('operators');
+const clear = document.getElementById('clear');
+const equal = document.getElementById('equal');
+const dot = document.getElementById('dot');
 
+let display
 
 allButtons.forEach(button => {
     button.addEventListener('click', (event) => {
         let clickedValue = event.target.value;
         calcAnswer.textContent += clickedValue;
-        // console.log(calcAnswer.textContent);
+        console.log(calcAnswer.textContent);
         if (clickedValue == "clear") {
             calcAnswer.textContent = "";
         }
         if (clickedValue == "delete") {
-            let display = calcAnswer.textContent;
+            display = calcAnswer.textContent;
             display = display.slice(0, -7);
             calcAnswer.textContent = display;
         }
-
-
-
+        if (clickedValue == ".") {
+            if (calcAnswer.textContent.includes(".")) {
+                button.disabled = true;
+            }
+        }
+        if (!(calcAnswer.textContent.includes("."))) {
+            dot.disabled = false;
+        }
 
 
 
