87c517b18b08fbd1c7ea5535607a5d0c1f12f9e2
Sean <seanpoulter@hotmail.com>
2022-07-22 21:19:55 -0400

    Working on add statements.

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const calcAnswer = document.querySelector('.answer');
 const allButtons = document.querySelectorAll('button');
-const operators = document.getElementsByClassName('operators');
+// const operators = document.getElementsByClassName('operators');
 const clear = document.getElementById('clear');
 const equal = document.getElementById('equal');
 const dot = document.getElementById('dot');
 
-let display
+const operators = ["+", "-", "*", "/"];
+
+let display 
+let previousValue = "";
+let currentValue = "";
 
 allButtons.forEach(button => {
     button.addEventListener('click', (event) => {
         let clickedValue = event.target.value;
-        calcAnswer.textContent += clickedValue;
+        
         console.log(calcAnswer.textContent);
+        if (clickedValue.includes("+")) {
+            previousValue = calcAnswer.textContent
+            // console.log(previousValue)
+            // console.log(calcAnswer.textContent);
+            if (previousValue != "") {
+
+                currentValue += clickedValue;
+                console.log(previousValue);
+                console.log(currentValue);
+                console.log(calcAnswer.textContent);
+            }
+        }
+
+        calcAnswer.textContent += clickedValue;
+
         if (clickedValue == "clear") {
             calcAnswer.textContent = "";
+            previousValue = "";
+            currentValue = "";
         }
         if (clickedValue == "delete") {
             display = calcAnswer.textContent;
@@ -28,10 +49,6 @@ allButtons.forEach(button => {
         if (!(calcAnswer.textContent.includes("."))) {
             dot.disabled = false;
         }
-
-
-
-
     })
 })
 
